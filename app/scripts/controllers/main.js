f549df099c4ffa300956361eab1e0c7e4f1b0c4c
TheeGrassHopper <almortada88@gmail.com>
2015-10-10 14:44:43 -0700

    link between the DOM markup and the scope of influence of a controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,12 +6,30 @@
  * @description
  * # MainCtrl
  * Controller of the movieManiaApp
- */
+*/
 angular.module('movieManiaApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	.controller('MainCtrl', function ($scope) {
+	    var movieList = [{
+	      'title': 'Usual Suspects',
+	      'image': 'http://ia.media-imdb.com/images/M/MV5BMzI1MjI5MDQyOV5BMl5BanBnXkFtZTcwNzE4Mjg3NA@@._V1_SX214_AL_.jpg',
+	      'description': 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.'
+	    },
+	    {
+	      'title': 'Star Wars',
+	      'image': 'http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX214_AL_.jpg',
+	      'description': 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empires world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vaders'
+	    },
+	    {
+	    'title': 'Dynasty',
+	    'image': 'http://ia.media-imdb.com/images/M/MV5BMTQ5MDY0NDc3MV5BMl5BanBnXkFtZTcwODE4MjgyMQ@@._V1_SY98_CR1,0,67,98_AL_.jpg',
+	    'description':	'Stevens ex-boyfriend Ted comes to town; a co-worker finds out. Fallon and Jeff elope; but to Fallon the marriage is a business deal'
+	    }];
+	    $scope.movies = movieList;
+  	}
+)
+	.controller('SubCtrl', function($scope){
+		$scope.title='Lots of Awesome movies Here: ' + $scope.movies.length ;
+	})
+	.controller('FormCtrl', function($scope) {
+  	console.log('hello this is a test');
   });
